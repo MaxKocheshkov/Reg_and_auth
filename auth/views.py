4e3a35cb06449e18d28379a943a42997bce661c3
Maxim Kocheshkov <gror_godfroy@mail.ru>
2020-08-23 19:51:34 +0300

    версия 1.1

--- auth/views.py
+++ auth/views.py
@@ -17,17 +17,4 @@ class SignUpView(generic.CreateView):
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
-# def signup(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#         username = form.cleaned_data.get('username')
-#         my_password = form.cleaned_data.get('password1')
-#         user = authenticate(username=username, password=my_password)
-#         login(request, user)
-#         return redirect('login')
-#     else:
-#         form = UserCreationForm()
-#         return render(request, 'signup.html', {'form': form})
 
